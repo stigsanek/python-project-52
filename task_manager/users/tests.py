279ca1b214b273a63e22ae1fbe45afd18ce34d29
stigsanek <stigsanek@yandex.ru>
2022-11-17 22:15:58 +0400

    Update tests for users

--- task_manager/users/tests.py
+++ task_manager/users/tests.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
-from django.urls import reverse_lazy
+from django.urls import reverse
+
+FAKE_PASSWORD = 'Fake_pass1!2@'
 
 
 class TestUsers(TestCase):
@@ -11,13 +13,55 @@ class TestUsers(TestCase):
         self.first_user = User.objects.get(pk=1)
         self.second_user = User.objects.get(pk=2)
 
-    def test_user_list_view(self):
+    def test_user_list(self):
         """Test for user list page"""
-        resp = self.client.get(reverse_lazy('users:list'))
+        resp = self.client.get(reverse('users:list'))
 
         self.assertEqual(resp.status_code, 200)
         self.assertQuerysetEqual(
-            resp.context['users'],
-            self.users,
+            qs=resp.context['users'],
+            values=self.users,
             ordered=False
         )
+
+    def test_sign_in(self):
+        """Test for sign in user"""
+        resp = self.client.post(
+            path=reverse('login'),
+            data={
+                'username': self.first_user.username,
+                'password': FAKE_PASSWORD
+            },
+            follow=True
+        )
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, 'Выход')
+        self.assertContains(resp, 'Вы залогинены')
+        self.assertNotContains(resp, 'Вход')
+
+    def test_sign_out(self):
+        """Test for sign out user"""
+        self.client.force_login(self.first_user)
+        resp = self.client.post(reverse('logout'))
+
+        self.assertEqual(resp.status_code, 302)
+        self.assertRedirects(resp, reverse('home'))
+
+    def test_create_user(self):
+        """Test for create user"""
+        resp = self.client.post(
+            path=reverse('users:create'),
+            data={
+                'username': 'test',
+                'password1': FAKE_PASSWORD,
+                'password2': FAKE_PASSWORD,
+            },
+            follow=True
+        )
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, 'Пользователь успешно зарегистрирован')
+
+        user = User.objects.filter(username='test').first()
+        self.assertIsNotNone(user)
