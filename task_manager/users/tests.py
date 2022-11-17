df1d61b5123674b9c1b6076c7e55163780ecaa0c
stigsanek <stigsanek@yandex.ru>
2022-11-17 18:02:12 +0400

    Add first test

--- task_manager/users/tests.py
+++ task_manager/users/tests.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse_lazy
 
-# Create your tests here.
+
+class TestUsers(TestCase):
+    fixtures = ['users.json']
+
+    def setUp(self):
+        self.users = User.objects.all()
+        self.first_user = User.objects.get(pk=1)
+        self.second_user = User.objects.get(pk=2)
+
+    def test_user_list_view(self):
+        """Test for user list page"""
+        resp = self.client.get(reverse_lazy('users:list'))
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertQuerysetEqual(
+            resp.context['users'],
+            self.users,
+            ordered=False
+        )
