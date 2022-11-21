51401cb70af57f6a2dca6a827ec27bc936b16429
stigsanek <stigsanek@yandex.ru>
2022-11-21 20:52:25 +0400

    Update admin options

--- task_manager/statuses/admin.py
+++ task_manager/statuses/admin.py
@@ -5,4 +5,5 @@ from task_manager.statuses import models
 
 @admin.register(models.Status)
 class StatusAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'created_at', 'updated_at')
+    search_fields = ('name',)
--- task_manager/tasks/admin.py
+++ task_manager/tasks/admin.py
@@ -5,5 +5,8 @@ from task_manager.tasks import models
 
 @admin.register(models.Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('name', 'status', 'author', 'executor')
-    list_filter = ('status',)
+    list_display = (
+        'name', 'status', 'author', 'executor', 'created_at', 'updated_at'
+    )
+    list_filter = ('status', 'author', 'executor', 'labels')
+    search_fields = ('name',)
